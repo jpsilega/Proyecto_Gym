49e0ae2ab5d407a8b47a9369b06601bf56476f01
Raudel-Costa <>
2019-08-09 11:55:33 -0300

    Creando componente header para separar el navbar

--- src/app/modules/FeatureModules/navbar-module/navbar.module.ts
+++ src/app/modules/FeatureModules/navbar-module/navbar.module.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header-component/header.component';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from '../../../app-routing.module';
 import { FooterComponent } from './footer-component/footer.component';
+import { HeaderNavbarComponent } from './components/header-navbar/header-navbar.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, HeaderNavbarComponent],
   imports: [
     CommonModule,
     FormsModule,
